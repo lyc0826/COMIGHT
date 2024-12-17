885344fbc1e36f42fa0ee6b8b30bdff797dec34e
Yuechen Lou <lyc0826@hotmail.com>
2024-12-17 14:43:23 +0800

    Improve some codes

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace COMIGHT.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.12.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.11.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -176,6 +176,18 @@ namespace COMIGHT.Properties {
             }
             set {
                 this["savingFolderPath"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("C:\\Program Files\\pandoc\\pandoc.exe")]
+        public string pandocPath {
+            get {
+                return ((string)(this["pandocPath"]));
+            }
+            set {
+                this["pandocPath"] = value;
             }
         }
         
@@ -440,18 +452,6 @@ namespace COMIGHT.Properties {
             }
             set {
                 this["enLineSpace"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("C:\\Program Files\\pandoc\\pandoc.exe")]
-        public string pandocPath {
-            get {
-                return ((string)(this["pandocPath"]));
-            }
-            set {
-                this["pandocPath"] = value;
             }
         }
     }
--- SettingDialog.xaml.cs
+++ SettingDialog.xaml.cs
@@ -30,38 +30,48 @@ namespace COMIGHT
             {
                 // 创建设置DataTable, 并添加列
                 settingTable = new DataTable();
-                settingTable.Columns.Add("Item", typeof(string));
-                settingTable.Columns.Add("Value", typeof(object));
 
-                // 添加设置项和默认值到设置DataTable
-                settingTable.Rows.Add("Saving Folder Path", Default.savingFolderPath);
-                settingTable.Rows.Add("Pandoc Application Path", Default.pandocPath);
+                settingTable.Columns.AddRange(new DataColumn[]
+                    {
+                        new DataColumn("Item", typeof(string)),
+                        new DataColumn("Value", typeof(object)),
+                    });
 
-                settingTable.Rows.Add("Chinese Title Font Name", Default.cnTitleFontName);
-                settingTable.Rows.Add("Chinese Title Font Size", Default.cnTitleFontSize);
-                settingTable.Rows.Add("Chinese Body Text Font Name", Default.cnBodyFontName);
-                settingTable.Rows.Add("Chinese Body Text Font Size", Default.cnBodyFontSize);
-                settingTable.Rows.Add("Chinese Heading Lv0 Font Name", Default.cnHeading0FontName);
-                settingTable.Rows.Add("Chinese Heading Lv0 Font Size", Default.cnHeading0FontSize);
-                settingTable.Rows.Add("Chinese Heading Lv1 Font Name", Default.cnHeading1FontName);
-                settingTable.Rows.Add("Chinese Heading Lv1 Font Size", Default.cnHeading1FontSize);
-                settingTable.Rows.Add("Chinese Heading Lv2 Font Name", Default.cnHeading2FontName);
-                settingTable.Rows.Add("Chinese Heading Lv2 Font Size", Default.cnHeading2FontSize);
-                settingTable.Rows.Add("Chinese Heading Lv3-4 Font Name", Default.cnHeading3_4FontName);
-                settingTable.Rows.Add("Chinese Heading Lv3-4 Font Size", Default.cnHeading3_4FontSize);
-                settingTable.Rows.Add("English Title Font Name", Default.enTitleFontName);
-                settingTable.Rows.Add("English Title Font Size", Default.enTitleFontSize);
-                settingTable.Rows.Add("English Body Text Font Name", Default.enBodyFontName);
-                settingTable.Rows.Add("English Body Text Font Size", Default.enBodyFontSize);
-                settingTable.Rows.Add("English Heading Font Name", Default.enHeadingFontName);
-                settingTable.Rows.Add("English Heading Font Size", Default.enHeadingFontSize);
-                settingTable.Rows.Add("Chinese Line Space", Default.cnLineSpace);
-                settingTable.Rows.Add("English Line Space", Default.enLineSpace);
+                // 定义设置列表，元素为值元组，包含设置项和值
+                List<(string item, object value)> lstSettings = new List<(string item, object value)>
+                    {
+                        ("Saving Folder Path", Default.savingFolderPath),
+                        ("Pandoc Application Path", Default.pandocPath),
+                        ("Chinese Title Font Name", Default.cnTitleFontName),
+                        ("Chinese Title Font Size", Default.cnTitleFontSize),
+                        ("Chinese Body Text Font Name", Default.cnBodyFontName),
+                        ("Chinese Body Text Font Size", Default.cnBodyFontSize),
+                        ("Chinese Heading Lv0 Font Name", Default.cnHeading0FontName),
+                        ("Chinese Heading Lv0 Font Size", Default.cnHeading0FontSize),
+                        ("Chinese Heading Lv1 Font Name", Default.cnHeading1FontName),
+                        ("Chinese Heading Lv1 Font Size", Default.cnHeading1FontSize),
+                        ("Chinese Heading Lv2 Font Name", Default.cnHeading2FontName),
+                        ("Chinese Heading Lv2 Font Size", Default.cnHeading2FontSize),
+                        ("Chinese Heading Lv3-4 Font Name", Default.cnHeading3_4FontName),
+                        ("Chinese Heading Lv3-4 Font Size", Default.cnHeading3_4FontSize),
+                        ("English Title Font Name", Default.enTitleFontName),
+                        ("English Title Font Size", Default.enTitleFontSize),
+                        ("English Body Text Font Name", Default.enBodyFontName),
+                        ("English Body Text Font Size", Default.enBodyFontSize),
+                        ("English Heading Font Name", Default.enHeadingFontName),
+                        ("English Heading Font Size", Default.enHeadingFontSize),
+                        ("Chinese Line Space", Default.cnLineSpace),
+                        ("English Line Space", Default.enLineSpace)
+                    };
 
+                foreach ((string item, object value) setting in lstSettings) // 遍历设置列表
+                {
+                    settingTable.Rows.Add(setting.item, setting.value); // 向设置DataTable添加设置项和值
+                }
 
-                // 设置 "Item" 列为主键以方便查找
-                settingTable.PrimaryKey = new[] { settingTable.Columns["Item"]! };
+                settingTable.PrimaryKey = new[] { settingTable.Columns["Item"]! }; // 设置 "Item" 列为主键以方便查找
             }
+
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, "Warning", MessageBoxButton.OK, MessageBoxImage.Information);
@@ -73,51 +83,30 @@ namespace COMIGHT
             try
             {
                 // 保存设置
-                DataRow? row = settingTable.Rows.Find("Saving Folder Path");
-                if (row != null) Default.savingFolderPath = (string)row["Value"];
-                row = settingTable.Rows.Find("Pandoc Application Path");
-                if (row != null) Default.pandocPath = (string)row["Value"];
 
-                row = settingTable.Rows.Find("Chinese Title Font Name");
-                if (row != null) Default.cnTitleFontName = (string)row["Value"];
-                row = settingTable.Rows.Find("Chinese Title Font Size");
-                if (row != null) Default.cnTitleFontSize = Convert.ToInt32(row["Value"]);
-                row = settingTable.Rows.Find("Chinese Body Text Font Name");
-                if (row != null) Default.cnBodyFontName = (string)row["Value"];
-                row = settingTable.Rows.Find("Chinese Body Text Font Size");
-                if (row != null) Default.cnBodyFontSize = Convert.ToInt32(row["Value"]);
-                row = settingTable.Rows.Find("Chinese Heading Lv0 Font Name");
-                if (row != null) Default.cnHeading0FontName = (string)row["Value"];
-                row = settingTable.Rows.Find("Chinese Heading Lv0 Font Size");
-                if (row != null) Default.cnHeading0FontSize = Convert.ToInt32(row["Value"]);
-                row = settingTable.Rows.Find("Chinese Heading Lv1 Font Name");
-                if (row != null) Default.cnHeading1FontName = (string)row["Value"];
-                row = settingTable.Rows.Find("Chinese Heading Lv1 Font Size");
-                if (row != null) Default.cnHeading1FontSize = Convert.ToInt32(row["Value"]);
-                row = settingTable.Rows.Find("Chinese Heading Lv2 Font Name");
-                if (row != null) Default.cnHeading2FontName = (string)row["Value"];
-                row = settingTable.Rows.Find("Chinese Heading Lv2 Font Size");
-                if (row != null) Default.cnHeading2FontSize = Convert.ToInt32(row["Value"]);
-                row = settingTable.Rows.Find("Chinese Heading Lv3-4 Font Name");
-                if (row != null) Default.cnHeading3_4FontName = (string)row["Value"];
-                row = settingTable.Rows.Find("Chinese Heading Lv3-4 Font Size");
-                if (row != null) Default.cnHeading3_4FontSize = Convert.ToInt32(row["Value"]);
-                row = settingTable.Rows.Find("English Title Font Name");
-                if (row != null) Default.enTitleFontName = (string)row["Value"];
-                row = settingTable.Rows.Find("English Title Font Size");
-                if (row != null) Default.enTitleFontSize = Convert.ToInt32(row["Value"]);
-                row = settingTable.Rows.Find("English Body Text Font Name");
-                if (row != null) Default.enBodyFontName = (string)row["Value"];
-                row = settingTable.Rows.Find("English Body Text Font Size");
-                if (row != null) Default.enBodyFontSize = Convert.ToInt32(row["Value"]);
-                row = settingTable.Rows.Find("English Heading Font Name");
-                if (row != null) Default.enHeadingFontName = (string)row["Value"];
-                row = settingTable.Rows.Find("English Heading Font Size");
-                if (row != null) Default.enHeadingFontSize = Convert.ToInt32(row["Value"]);
-                row = settingTable.Rows.Find("Chinese Line Space");
-                if (row != null) Default.cnLineSpace = Convert.ToInt32(row["Value"]);
-                row = settingTable.Rows.Find("English Line Space");
-                if (row != null) Default.enLineSpace = Convert.ToInt32(row["Value"]);
+                Default.savingFolderPath = Convert.ToString(settingTable.Rows.Find("Saving Folder Path")?["Value"]);
+                Default.pandocPath = Convert.ToString(settingTable.Rows.Find("Pandoc Application Path")?["Value"]);
+
+                Default.cnTitleFontName = Convert.ToString(settingTable.Rows.Find("Chinese Title Font Name")?["Value"]);
+                Default.cnTitleFontSize = Convert.ToInt32(settingTable.Rows.Find("Chinese Title Font Size")?["Value"]);
+                Default.cnBodyFontName = Convert.ToString(settingTable.Rows.Find("Chinese Body Text Font Name")?["Value"]);
+                Default.cnBodyFontSize = Convert.ToInt32(settingTable.Rows.Find("Chinese Body Text Font Size")?["Value"]);
+                Default.cnHeading0FontName = Convert.ToString(settingTable.Rows.Find("Chinese Heading Lv0 Font Name")?["Value"]);
+                Default.cnHeading0FontSize = Convert.ToInt32(settingTable.Rows.Find("Chinese Heading Lv0 Font Size")?["Value"]);
+                Default.cnHeading1FontName = Convert.ToString(settingTable.Rows.Find("Chinese Heading Lv1 Font Name")?["Value"]);
+                Default.cnHeading1FontSize = Convert.ToInt32(settingTable.Rows.Find("Chinese Heading Lv1 Font Size")?["Value"]);
+                Default.cnHeading2FontName = Convert.ToString(settingTable.Rows.Find("Chinese Heading Lv2 Font Name")?["Value"]);
+                Default.cnHeading2FontSize = Convert.ToInt32(settingTable.Rows.Find("Chinese Heading Lv2 Font Size")?["Value"]);
+                Default.cnHeading3_4FontName = Convert.ToString(settingTable.Rows.Find("Chinese Heading Lv3-4 Font Name")?["Value"]);
+                Default.cnHeading3_4FontSize = Convert.ToInt32(settingTable.Rows.Find("Chinese Heading Lv3-4 Font Size")?["Value"]);
+                Default.enTitleFontName = Convert.ToString(settingTable.Rows.Find("English Title Font Name")?["Value"]);
+                Default.enTitleFontSize = Convert.ToInt32(settingTable.Rows.Find("English Title Font Size")?["Value"]);
+                Default.enBodyFontName = Convert.ToString(settingTable.Rows.Find("English Body Text Font Name")?["Value"]);
+                Default.enBodyFontSize = Convert.ToInt32(settingTable.Rows.Find("English Body Text Font Size")?["Value"]);
+                Default.enHeadingFontName = Convert.ToString(settingTable.Rows.Find("English Heading Font Name")?["Value"]);
+                Default.enHeadingFontSize = Convert.ToInt32(settingTable.Rows.Find("English Heading Font Size")?["Value"]);
+                Default.cnLineSpace = Convert.ToInt32(settingTable.Rows.Find("Chinese Line Space")?["Value"]);
+                Default.enLineSpace = Convert.ToInt32(settingTable.Rows.Find("English Line Space")?["Value"]);
 
                 Default.Save(); // 保存默认设置
 
