5b5e238e8e9491c63ddb48164cb07b7019bd06aa
Yuechen Lou <lyc0826@hotmail.com>
2025-05-27 15:20:53 +0800

    Update some comments

--- DataGridWindow.xaml.cs
+++ DataGridWindow.xaml.cs
@@ -13,10 +13,10 @@ namespace COMIGHT
     /// </summary>
     public partial class DataGridWindow : Window
     {
-        private DataTable dataTable; // 使用私有字段存储当前DataGrid绑定的DataTable
-        private string windowTitle = string.Empty; //  定义窗口标题
+        private DataTable dataTable; // 定义DataTable变量
+        private string windowTitle = string.Empty; // 定义窗口标题变量
 
-        // 默认构造函数（可选，如果总是通过参数传入，可以移除）
+        // 默认构造函数
         public DataGridWindow()
         {
             InitializeComponent();
@@ -24,7 +24,7 @@ namespace COMIGHT
             myDataGrid.ItemsSource = dataTable.DefaultView; //  将DataTable绑定到DataGrid
         }
 
-        // 新增的构造函数，允许传入 DataTable
+        // 新增的构造函数，允许传入DataTable
         public DataGridWindow(string windowTitle, DataTable dataTable) : this() // 调用默认构造函数进行初始化
         {
             this.Title = this.windowTitle = windowTitle; //  设置窗口标题，并赋值给windowTitle字段
@@ -43,7 +43,7 @@ namespace COMIGHT
             {
                 string targetFolderPath = appSettings.SavingFolderPath; //获取目标文件夹路径
                 string targetExcelFile = Path.Combine(targetFolderPath!, $"{CleanFileAndFolderName(windowTitle, 40)}.xlsx"); //获取目标Excel工作簿文件路径全名信息
-                WriteDataTableIntoExcelWorkbook(new List<DataTable>() { dataTable }, targetExcelFile);
+                WriteDataTableIntoExcelWorkbook(new List<DataTable>() { dataTable }, targetExcelFile); //将DataTable数据写入Excel工作簿
 
                 ShowSuccessMessage();
             }
