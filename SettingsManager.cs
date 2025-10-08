9e28bc4e350ae88c9f472cdb59e212a65bdb71a2
Yuechen Lou <lyc0826@hotmail.com>
2025-10-08 11:00:52 +0800

    Update comments in SettingsManager

--- SettingsManager.cs
+++ SettingsManager.cs
@@ -5,7 +5,7 @@ using System.IO;
 namespace COMIGHT
 {
 
-    public class SettingsManager<T> where T : new() // 定义一个泛型类，泛型参数T必须有一个无参数的公共构造函数，允许在类内部通过 new T()创建T类型的新实例。
+    public class SettingsManager<T> where T : new() // 定义设置管理器泛型类，泛型参数T必须有一个无参数的公共构造函数，允许在类内部通过 new T()创建T类型的新实例。
     {
         private readonly string settingsFilePath; // 定义设置文件路径变量
         private T settings = new T(); // 定义设置对象变量
@@ -13,10 +13,10 @@ namespace COMIGHT
         public SettingsManager(string settingsFilePath)
         {
             this.settingsFilePath = settingsFilePath; // 从方法参数中获取设置JSON文件路径
-            LoadSettings();
+            LoadSettings(); // 从设置JSON文件中加载设置
         }
 
-        // 加载设置
+        // 从设置JSON文件中加载设置
         private void LoadSettings()
         {
             if (File.Exists(settingsFilePath)) // 如果JSON文件存在
@@ -31,13 +31,13 @@ namespace COMIGHT
 
         }
 
-        // 获取设置
+        // 读取设置并返回给调用者
         public T GetSettings()
         {
             return JsonConvert.DeserializeObject<T>(JsonConvert.SerializeObject(settings)) ?? new T(); //将内部设置对象序列化后再反序列化，形成深拷贝（与原对象无引用关系），赋值给外部调用者（如果序列化失败，则得到默认初始化对象）
         }
 
-        // 保存设置
+        // 保存新设置
         public void SaveSettings(T newSettings)
         {
             //将当前设置对象和新设置对象序列化为JSON字符串
